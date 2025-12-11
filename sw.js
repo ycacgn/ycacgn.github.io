a69052140cca97526244e916ecdcb3092d6f25f2
ycacgn <llxgz202209@163.com>
2025-12-11 23:43:34 +0800

    Site updated: 2025-12-11 23:43:34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765466354443";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765467813762";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
